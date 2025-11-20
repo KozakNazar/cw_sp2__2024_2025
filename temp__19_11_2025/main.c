8836c38a48209695425c35ba972f55da0457afd9
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2025-11-20 13:36:17 +0200

    Add files via upload

---
+++ temp__19_11_2025/main.c
@@ -0,0 +1,5 @@
+extern int call_main_spirit(int argc, char* argv[]);
+
+int main(int argc, char* argv[]) {
+	return call_main_spirit(argc, argv);
+}
---
+++ temp__19_11_2025/one_file__wrapper.cpp
@@ -0,0 +1,5 @@
+extern int main_spirit(int argc, char* argv[]);
+
+extern "C" int call_main_spirit(int argc, char* argv[]) {
+    return main_spirit(argc, argv);
+}
