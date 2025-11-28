47eb9765fef6dc98a69cb59d9ec8bf09375908e8
KozakNazar <55637421+KozakNazar@users.noreply.github.com>
2025-11-28 05:40:10 +0200

    Add files via upload

---
+++ 28_11_2025/ComputeByStack.hpp
@@ -0,0 +1,112 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "MyStack.h"
+#include "ComputeByStack.h"
+
+int assignFunction(int* arg1, int arg2) {
+    return *arg1 = arg2;
+}
+
+void assign(MyStack<Element>& operandsStack, int(*assignFunctionWithReturn)(int*, int)) {
+    int* arg1 = NULL, arg2 = 0;
+    if (operandsStack.top().data.value == VARIABLE) {
+        arg2 = *operandsStack.top().data.variablePtr;
+    }
+    else if (operandsStack.top().data.value == VALUE) {
+        arg2 = operandsStack.top().data.value;
+    }
+    else {
+        printf("error");
+        exit(0);
+    }
+    operandsStack.pop();
+
+    if (operandsStack.top().data.value == VARIABLE)
+        arg1 = operandsStack.top().data.variablePtr;
+    else {
+        printf("error");
+        exit(0);
+    }
+    operandsStack.pop();
+
+    Element result;
+    result.type = VALUE;
+    result.data.value = assignFunctionWithReturn(arg1, arg2);
+    operandsStack.push(result);
+}
+
+int sumFunction(int arg1, int arg2) {
+    return arg1 + arg2;
+}
+
+int subFunction(int arg1, int arg2) {
+    return arg1 - arg2;
+}
+
+int divFunction(int arg1, int arg2) {
+    return arg1 / arg2;
+}
+
+int mulFunction(int arg1, int arg2) {
+    return arg1 * arg2;
+}
+
+int modFunction(int arg1, int arg2) {
+    return arg1 % arg2;
+}
+
+void binary(MyStack<Element>& operandsStack, int(*binaryFunction)(int, int)) {
+    int arg1 = 0, arg2 = 0;
+    if (operandsStack.top().data.value == VARIABLE) {
+        arg2 = operandsStack.top().data.value;
+    }
+    else if (operandsStack.top().data.value == VALUE) {
+        arg2 = *operandsStack.top().data.variablePtr;
+    }
+    else {
+        printf("error");
+        exit(0);
+    }
+    operandsStack.pop();
+
+    if (operandsStack.top().data.value == VARIABLE) {
+        arg1 = operandsStack.top().data.value;
+    }
+    else if (operandsStack.top().data.value == VALUE) {
+        arg1 = *operandsStack.top().data.variablePtr;
+    }
+    else {
+        printf("error");
+        exit(0);
+    }
+    operandsStack.pop();
+
+    Element result;
+    result.type = VALUE;
+    result.data.value = binaryFunction(arg1, arg2);
+    operandsStack.push(result);
+}
+
+int notFunction(int arg) {
+    return !arg;
+}
+
+void unary(MyStack<Element>& operandsStack, int(*unaryFunction)(int)) {
+    int arg = 0;
+    if (operandsStack.top().data.value == VARIABLE) {
+        arg = operandsStack.top().data.value;
+    }
+    else if (operandsStack.top().data.value == VALUE) {
+        arg = *operandsStack.top().data.variablePtr;
+    }
+    else {
+        printf("error");
+        exit(0);
+    }
+    operandsStack.pop();
+
+    Element result;
+    result.type = VALUE;
+    result.data.value = unaryFunction(arg);
+    operandsStack.push(result);
+}
